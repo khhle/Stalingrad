ded01c13f5fa8768ba8e703849473792f630e9f3
Matthew Wang <majwang@ucsc.edu>
2014-06-05 13:22:35 -0700

    todolist

---
+++ Assets/todolist.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+/*-airplane blocking shots
+-add movement left or indicator
+-rename units
+-counterattack indicator
+-splash damage
+-add city terrain 
+-balancing for costs/stats
+-fix lag
+-replace check mark to flags
+-add BGM ingame
+-add menu screen
+*/
+}
+
+function Update () {
+
+}
