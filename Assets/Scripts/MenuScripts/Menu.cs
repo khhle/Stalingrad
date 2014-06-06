4c8c83e5a317d91fa58c317e16b9e4b1e9f619e2
Brian Wichers <bwichers@ucsc.edu>
2014-06-06 14:18:56 -0700

    Menu screen

---
+++ Assets/Scripts/MenuScripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public AudioClip hoverOver;
+	private string lastTip = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		if(GUI.Button (new Rect (430, 275, 100, 50), new GUIContent("PLAY", "play"))){
+			Application.LoadLevel("Selection");
+		}
+		if(Event.current.type == EventType.Repaint && GUI.tooltip != lastTip){
+			if (GUI.tooltip == "play")
+				audio.PlayOneShot (hoverOver);
+			if (lastTip != "")
+				lastTip = "";
+			else
+				lastTip = "play";
+		}
+	}
+}
