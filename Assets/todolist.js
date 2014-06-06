7eb9325260d87ebf39790b7668d4b42ef25152f4
khhle <khhle@ucsc.edu>
2014-06-06 05:21:12 -0700

    update todo list

--- Assets/todolist.js
+++ Assets/todolist.js
@@ -13,6 +13,28 @@ function Start () {
 -add BGM ingame
 -add menu screen
 */
+
+
+
+
+
+
+/*
+Brian!!!!! why do you put Soviet army on the right side =.= 
+According to google, German army was on the right side, the upper part of the river, and Soviet was attacking from the lower part. 
+
+Anyways, there is a river in Stalingrad, but I don't know how we gonna implement it. 
+Some units can't move over river, but they can shoot across it. Then air units can fly over river. So....? 
+
+
+Bug notice:
+rusian bomer uses german bomber prefab ??
+array out of bound happends sometimes
+flipping camera will not flip building unit. Try to implement, but slowing down the game and laggy...
+
+
+*/
+
 }
 
 function Update () {
